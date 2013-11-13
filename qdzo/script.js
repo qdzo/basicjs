01ea6b34aa20223e12c788d030ca2855d0c1e667
qdzo <jetban@mail.ru>
2013-11-13 22:26:29 +0400

    js cource first exercsise

---
+++ qdzo/script.js
@@ -0,0 +1,13 @@
+var login  = prompt("Enter a login:");
+if (login != "ded") {
+	alert("отказ в авторизации!");
+}
+else {
+	var pass = prompt("enter a pass:");
+	if(pass != "dedded"){
+		alert("отказ в авторизации");
+	}
+	else
+		alert("you are merried, ded!");
+}
+
