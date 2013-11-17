b5a5afac3d0bdb9b055abf3b238a1025e401a1c7
qdzo <jetban@mail.ru>
2013-11-17 14:06:31 +0400

    add password re-enter option

--- qdzo/script.js
+++ qdzo/script.js
@@ -3,11 +3,19 @@ if (login != "ded") {
 	alert("отказ в авторизации!");
 }
 else {
-	var pass = prompt("enter a pass:");
-	if(pass != "dedded"){
-		alert("отказ в авторизации");
-	}
-	else
-		alert("you are merried, ded!");
+	var iterator = 5;
+	do{
+		var pass = prompt("enter a pass:");
+		if(pass != "dedded"){
+			alert("wrong password, enter once again!");
+			iterator--;
+		}
+		else{
+			alert("you are merried, ded!");
+			break;
+		}
+			
+	} while(iterator>0)
+	
 }
 
