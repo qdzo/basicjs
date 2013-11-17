2d0fa3110e882f5530f221e1cc883eb02d9e504c
qdzo <jetban@mail.ru>
2013-11-17 17:58:48 +0400

    add a switch orarator to login-form

--- qdzo/script.js
+++ qdzo/script.js
@@ -1,21 +1,54 @@
 var login  = prompt("Enter a login:");
-if (login != "ded") {
-	alert("отказ в авторизации!");
-}
-else {
-	var iterator = 5;
-	do{
-		var pass = prompt("enter a pass:");
-		if(pass != "dedded"){
-			alert("wrong password, enter once again!");
-			iterator--;
-		}
-		else{
-			alert("you are merried, ded!");
-			break;
-		}
+switch (login) {
+	case "ded": 
+				var iterator = 5;
+				do{
+					var pass = prompt("enter a pass:");
+					if(pass != "dedded"){
+						alert("wrong password, enter once again!");
+						iterator--;
+					}
+					else{
+						alert("you are merried, ded!");
+						break;
+					}
 			
-	} while(iterator>0)
-	
+				} while(iterator>0)
+				break;
+	case "bob" :
+				var iterator = 5;
+				do{
+					var pass = prompt("enter a pass:");
+					if(pass != "bobbob"){
+						alert("wrong password, enter once again!");
+						iterator--;
+					}
+					else{
+						alert("I know where you were last summer!");
+						break;
+					}
+			
+				} while(iterator>0)
+				break;			
+	case "ann" :
+				var iterator = 5;
+				do{
+					var pass = prompt("enter a pass:");
+					if(pass != "annann"){
+						alert("wrong password, enter once again!");
+						iterator--;
+					}
+					else{
+						alert("Hello Ann, would you go to the theatre");
+						break;
+					}
+			
+				} while(iterator>0)
+				break;			
+
+	default: alert("отказ в авторизации!");
 }
+
+	
+
 
